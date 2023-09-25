864413f03bba5ed65c3dbb5bce144680e1a41a48
abdulkadirgul <abdulkadirgull1@gmail.com>
2023-09-25 22:11:23 +0300

    Fanout Exchange Type Added.

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -16,17 +16,18 @@ namespace RabbitMQ.Publisher
 
             var channel = connection.CreateModel();
 
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
 
             channel.QueueDeclare("hello-queue", true, false, false);
 
             Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
 
-                string message =$"Message {x}";
+                string message =$"log {x}";
 
                 var messageBody = Encoding.UTF8.GetBytes(message);
 
-                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+                channel.BasicPublish("logs-fanout", "", null, messageBody);
 
                 Console.WriteLine($"Mesajınız Gönderildi : { message }");
 
